c41e3d5289dc50831b31fa292ab37296fd70952a
vikashpandey770 <vikashshrinivaspandey@gmail.com>
2026-06-20 18:08:35 +0530

    commit changes

---
+++ Assigments/Introduction to OOPS Programming/session.7.5.cpp
@@ -0,0 +1,27 @@
+//5. Given a file called insta_followers.txt containing Instagram usernames (one per line), write a program to
+// count and display the total number of followers listed in the file.<br><br><em><strong>Constraint:
+// </strong> Do not use any array or vector to store the names—just count as you read.</em>
+
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+	
+	ofstream file("insta_followers.txt"){
+	
+	string usernames;
+	string sp;
+	
+	cout<<"enter a username:";
+	getline(cin,usernames);
+	
+	
+	}
+		
+	}
+	
+	file.close();
+	return 0;
+}
